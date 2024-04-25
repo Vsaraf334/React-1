daa4fa33ff8ea8fe8c828baa084bbddbdf1cf756
Varun <varun.aggarwal789@mail.com>
2024-04-25 23:14:39 +0530

    "begin with some Tailwind CSS"

---
+++ 01ViteProject/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-center text-white">
+        So I started to walk into the water...
+      </h1>
+    </>
+  );
+}
+
+export default App;
