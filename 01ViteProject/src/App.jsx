c3f17045671e33a0365a5a3ace2249daacf257b0
Varun <varun.aggarwal789@mail.com>
2024-04-26 01:07:43 +0530

    complete the color change project

--- 01ViteProject/src/App.jsx
+++ 01ViteProject/src/App.jsx
@@ -1,14 +1,38 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const arr = [
+    { red: "bg-red-600" },
+    { blue: "bg-blue-600" },
+    { green: "bg-green-600" },
+    { purple: "bg-purple-600" },
+    { black: "bg-black" },
+  ];
+
+  const [bgColor, setBgColor] = useState("bg-white");
+
+  const changeColor = (val) => {
+    setBgColor(Object.values(val));
+  };
 
   return (
-    <>
-      <h1 className="text-center text-white">
-        So I started to walk into the water...
-      </h1>
-    </>
+    <div className={` ${bgColor} min-h-screen py-1`}>
+      <div className="w-3/4 h-20 bg-slate-700 mx-auto my-52 rounded-3xl flex justify-between">
+        {arr.map((val) => {
+          return (
+            <button
+              key={Object.keys(val)}
+              onClick={() => changeColor(val)}
+              className={`w-[130px] text-white px-6 ${Object.values(
+                val
+              )} m-4 rounded-2xl`}
+            >
+              {Object.keys(val)}
+            </button>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
